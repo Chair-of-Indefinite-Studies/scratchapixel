f01671b6c8032ee540927318a9682137a0f6a942
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-03-30 14:40:11 +0200

    Introduce dot product for Vec3

--- scratchapixel/src/geometry/vector.rs
+++ scratchapixel/src/geometry/vector.rs
@@ -1,5 +1,6 @@
 use std::ops::{Mul,Add};
-use num::traits::Zero;
+use num::traits::{Zero,Float};
+
 
 #[derive(PartialEq,Debug)]
 pub struct Vec3<T> where T: Mul<T, Output = T> + Add<T, Output = T> + Zero + Copy + Clone {
@@ -20,7 +21,17 @@ impl <T> Vec3<T> where T : Mul<T, Output = T> + Add<T, Output = T> + Zero + Copy
     pub fn diagonal(xx: T) -> Vec3<T> {
         Vec3::new(xx, xx, xx)
     }
+}
 
+impl <T> Mul<Vec3<T>> for Vec3<T> where T: Mul<T, Output = T> + Add<T, Output = T> + Zero + Copy + Clone {
+    type Output = T;
+
+    fn mul(self, rhs: Vec3<T>) -> T {
+        return self.x * rhs.x + self.y * rhs.y + self.z * rhs.z
+    }
+}
+
+impl <T> Vec3<T> where T: Mul<T, Output=T> + Add<T, Output=T> + Zero + Float + Clone + Copy {
     pub fn norm(self) -> T {
         (self.x * self.x + self.y * self.y + self.z * self.z)
     }
@@ -49,6 +60,16 @@ mod tests {
         let v: Vec3<f64> = Vec3::diagonal(1.0);
 
         assert_eq!(v, Vec3::<f64>{ x: 1.0, y: 1.0, z: 1.0 });
+    }
+
+    #[test]
+    fn should_know_dot_product() {
+        let u: Vec3<f64> = Vec3::diagonal(1.0);
+        let v: Vec3<f64> = Vec3::new(0.0, 1.0, 2.0);
+
+        let dot_product: f64 = u * v;
+
+        assert_eq!(dot_product, 3.0);
     }
 
     #[test]
