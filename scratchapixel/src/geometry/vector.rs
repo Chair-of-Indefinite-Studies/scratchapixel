d33c4c6e5d9a0969db104113e028fbe5bcfaf888
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-03-30 08:39:29 +0200

    Create a diagonal constructor

--- scratchapixel/src/geometry/vector.rs
+++ scratchapixel/src/geometry/vector.rs
@@ -13,6 +13,10 @@ impl <T> Vec3<T> where T : Mul<T, Output = T> + Add<T, Output = T> + Zero + Copy
         Vec3 { x: T::zero(), y: T::zero(), z: T::zero() }
     }
 
+    pub fn diagonal(xx: T) -> Vec3<T> {
+        Vec3 { x: xx, y: xx, z: xx }
+    }
+
     pub fn norm(self) -> T {
         (self.x * self.x + self.y * self.y + self.z * self.z)
     }
@@ -26,7 +30,14 @@ mod tests {
     fn should_initialize_to_zero_vector() {
         let v: Vec3<f64> = Vec3::new();
 
-        assert_eq!(v, Vec3::<f64>{ x: 0.0, y: 0.0, z: 0.0});
+        assert_eq!(v, Vec3::<f64>{ x: 0.0, y: 0.0, z: 0.0 });
+    }
+
+    #[test]
+    fn should_initialize_to_diagonal_vector() {
+        let v: Vec3<f64> = Vec3::diagonal(1.0);
+
+        assert_eq!(v, Vec3::<f64>{ x: 1.0, y: 1.0, z: 1.0 });
     }
 
     #[test]
