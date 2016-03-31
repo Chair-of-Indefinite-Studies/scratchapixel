4e33bda2a18f8304aab0ae877fba6a683b02ae49
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-03-31 09:30:17 +0200

    Implement Index trait for Matrix44

--- scratchapixel/src/geometry/matrix.rs
+++ scratchapixel/src/geometry/matrix.rs
@@ -1,4 +1,5 @@
 use num::traits::{Zero};
+use std::ops::Index;
 
 #[derive(PartialEq,Debug)]
 pub struct Matrix44<T> {
@@ -16,6 +17,14 @@ impl <T> Matrix44<T> where T: Zero {
 
     pub fn new(mm: [[T; 4]; 4]) -> Matrix44<T> {
         Matrix44 { m : mm }
+    }
+}
+
+impl <T> Index<usize> for Matrix44<T> where T: Zero {
+    type Output = [T; 4];
+
+    fn index(&self, index: usize) -> &[T; 4] {
+       &(self.m[index])
     }
 }
 
@@ -50,5 +59,18 @@ mod tests {
             [0.0, 0.0, 3.0, 0.0],
             [0.0, 0.0, 4.0, 0.0],
             ]});
+    }
+
+    #[test]
+    fn should_index_rows() {
+        let m: Matrix44<f64> = Matrix44::new([
+            [1.0, 2.0, 3.0, 4.0],
+            [2.0, 3.0, 4.0, 5.0],
+            [3.0, 4.0, 5.0, 6.0],
+            [4.0, 5.0, 6.0, 7.0], ]);
+
+        let row = m[1];
+
+        assert_eq!(row, [2.0, 3.0, 4.0, 5.0]);
     }
 }
