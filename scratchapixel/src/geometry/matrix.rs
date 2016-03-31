08b709e0ac704cb2bde21e2e10ef6c3c45db7329
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-03-31 09:08:09 +0200

    Create a matrix constructor

--- scratchapixel/src/geometry/matrix.rs
+++ scratchapixel/src/geometry/matrix.rs
@@ -13,6 +13,10 @@ impl <T> Matrix44<T> where T: Zero {
             [T::zero(), T::zero(), T::zero(), T::zero()],
             [T::zero(), T::zero(), T::zero(), T::zero()], ]}
     }
+
+    pub fn new(mm: [[T; 4]; 4]) -> Matrix44<T> {
+        Matrix44 { m : mm }
+    }
 }
 
 #[cfg(test)]
@@ -28,6 +32,23 @@ mod tests {
             [0.0, 0.0, 0.0, 0.0],
             [0.0, 0.0, 0.0, 0.0],
             [0.0, 0.0, 0.0, 0.0],
+            ]});
+    }
+
+    #[test]
+    fn should_be_initialized_by_coefficients() {
+        let m: Matrix44<f64> = Matrix44::new([
+            [1.0, 0.0, 0.0, 0.0],
+            [0.0, 2.0, 0.0, 0.0],
+            [0.0, 0.0, 3.0, 0.0],
+            [0.0, 0.0, 4.0, 0.0],
+            ]);
+
+        assert_eq!(m, Matrix44::<f64> { m: [
+            [1.0, 0.0, 0.0, 0.0],
+            [0.0, 2.0, 0.0, 0.0],
+            [0.0, 0.0, 3.0, 0.0],
+            [0.0, 0.0, 4.0, 0.0],
             ]});
     }
 }
